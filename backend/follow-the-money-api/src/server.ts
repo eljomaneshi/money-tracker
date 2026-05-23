19af36a7389832d4ec9b71a0db4b9673fc426e33
Eljo Maneshi <111017780+eljomaneshi@users.noreply.github.com>
2026-05-23 21:51:03 +0200

    bind server to 0.0.0.0 for railway

--- backend/follow-the-money-api/src/server.ts
+++ backend/follow-the-money-api/src/server.ts
@@ -5,8 +5,8 @@ dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
 import app from "./app";
 
-const PORT = process.env.PORT || 4000;
+const PORT = Number(process.env.PORT) || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
